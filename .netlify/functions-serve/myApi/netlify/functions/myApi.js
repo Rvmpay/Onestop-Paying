39b4cb4ad60ccfac87ad4a0b57f0a2714b225250
RVMPAY <rvmpay@RVMPAYs-MacBook-Air.local>
2025-05-08 14:33:34 +0530

    setup netlify for deploy apis

---
+++ .netlify/functions-serve/myApi/netlify/functions/myApi.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// netlify/functions/myApi.js
+var __awaiter = exports && exports.__awaiter || function(thisArg, _arguments, P, generator) {
+  function adopt(value) {
+    return value instanceof P ? value : new P(function(resolve) {
+      resolve(value);
+    });
+  }
+  return new (P || (P = Promise))(function(resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+    function step(result) {
+      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+    }
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+var handler = (event, context) => __awaiter(void 0, void 0, void 0, function* () {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Hello from TypeScript API!" })
+  };
+});
+exports.handler = handler;
+//# sourceMappingURL=myApi.js.map
