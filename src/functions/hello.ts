88a7c7386f6f8a7140c51a769e62cc0929dd220f
RVMPAY <rvmpay@RVMPAYs-MacBook-Air.local>
2025-05-08 16:32:52 +0530

    update netlify.toml

--- src/functions/hello.ts
+++ src/functions/hello.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 import serverless from 'serverless-http';
-import upiRoutes from '../../src/routes/upitranscation.route'; // adjust path
+import upiRoutes from '../routes/upitranscation.route'; // adjust path
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/v1/pg', upiRoutes);
 app.use('/api/v1/payout', upiRoutes);
+
+app.get('/health', (req, res) => {
+  res.send('API is alive');
+});
 
 export const handler = serverless(app);
