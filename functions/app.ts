4892e77e43eaa0abc1c561b07812b87a25032e16
RVMPAY <rvmpay@RVMPAYs-MacBook-Air.local>
2025-05-10 10:22:12 +0530

    add serverless ahndler in app.ts

--- functions/app.ts
+++ functions/app.ts
@@ -1,8 +1,6 @@
 import serverless from 'serverless-http';
-import express from 'express';
 import app from '../src/server';
 
-// Ensure body parsing works in serverless environments
-app.use(express.json());
-
-export const handler = serverless(app);
+export const handler = serverless(app, {
+  request: { rawBody: false }
+});
